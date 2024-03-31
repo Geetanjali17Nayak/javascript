7022a5472ce46cc9be26e182c084ff5ccf6e4ae3
Geetanjali17Nayak <117501359+Geetanjali17Nayak@users.noreply.github.com>
2024-03-31 18:47:57 +0000

    Datatypes in js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let name="Geetanjali"
+let age=20
+let isLoggedIn=false
+let state=null
+let city; //undefined
+
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//null => standalone value
+//undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof "Geetanjali")
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
+
