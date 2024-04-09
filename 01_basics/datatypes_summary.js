d295f4e771d1795a7e9b8fe9bc8261e2ddb98d24
Geetanjali17Nayak <117501359+Geetanjali17Nayak@users.noreply.github.com>
2024-04-09 10:46:37 +0000

    summary of datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+//Primitive
+
+// 7 types:- String ,Number,Boolean,null,undefined,Symbol(unique),BigInt
+
+const score=100;
+const scorevalue=100.8
+const isloggedIn=false
+let email  //Undefined
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id==anotherId);  //Output:- False
+
+
+
+//Reference (Non PRimitive)
+
+//Array,Objects,Functions
